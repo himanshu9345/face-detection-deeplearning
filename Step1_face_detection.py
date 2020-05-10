316f36d715a00a1b68240a74c790b43101134cb5
himanshu9345 <hphimanshu6@gmail.com>
2020-05-10 15:49:32 -0400

    updated wiki, Step2 extracting facial feature

--- Step1_face_detection.py
+++ Step1_face_detection.py
@@ -2,6 +2,7 @@ import cv2
 import face_recognition
 
 # loading image into np array
+# this library uses HOG to detect faces in a image
 image = face_recognition.load_image_file("images/people.jpg")
 
 # getting face loactions
@@ -13,11 +14,12 @@ image = cv2.imread("images/people.jpg")
 height, width, size = image.shape
 for face_location in face_locations:
     top, right, bottom, left = face_location
-    print(face_location)
+    # print(face_location)
 
     top_left = left, top
     bottom_right = right, bottom
-    print(top_left, bottom_right)
+    # draw rectangle around the faces
     cv2.rectangle(image, top_left, bottom_right, (0, 0, 255), 2)
-    cv2.imshow("output", image)
+    cv2.imshow("Face Detected", image)
     k = cv2.waitKey(0)
+cv2.imwrite("images/step1_output.jpg", image)
---
+++ Step2_extacting_facial_feature.py
@@ -0,0 +1,25 @@
+import cv2
+import face_recognition
+import numpy as np
+
+# loading image into np array
+# this library uses HOG to detect faces in a image
+image = face_recognition.load_image_file("images/people.jpg")
+
+# getting facial feature in an image
+face_landmarks_list = face_recognition.face_landmarks(image)
+
+print("Found {} face(s) in this image.".format(len(face_landmarks_list)))
+
+image = cv2.imread("images/people.jpg")
+
+for face_landmarks in face_landmarks_list:
+    for name, list_of_points in face_landmarks.items():
+        print("Feature detected is {}".format(name))
+        cv2.polylines(image, [np.array(list_of_points)] \
+            , isClosed=False, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
+    # press q to see facial features of new face
+    cv2.imshow("Facial Feature", image)
+    k = cv2.waitKey(0)
+# dumping ouput to a image file
+cv2.imwrite("images/step2_output.jpg", image)
